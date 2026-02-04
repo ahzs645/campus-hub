3d4591bbb0e76e4d691702270ed94b5964b646f9
Ahmad Jalil <ahzs645@gmail.com>
2026-02-04 13:51:52 -0800

    Fix exported asset paths

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
